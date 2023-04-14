6afb9c13b1969bce5222d4223c20b3e77a758194
IceRaptor <gkitts@gmail.com>
2023-04-14 16:54:15 -0400

    - Eliminate harmony private fields

--- FieldRepairs/FieldRepairs/Patches/AbstractActorPatches.cs
+++ FieldRepairs/FieldRepairs/Patches/AbstractActorPatches.cs
@@ -9,21 +9,20 @@ namespace FieldRepairs.Patches
     [HarmonyPatch(typeof(AbstractActor), "OnEffectEnd")]
     static class AbstractActor_OnEffectEnd
     {
-        static void Prefix(ref bool __runOriginal, AbstractActor __instance, Effect effect, List<Effect> ___markEffects, int ___StealthPipsPrevious)
+        static void Prefix(ref bool __runOriginal, AbstractActor __instance, Effect effect)
         {
             if (!__runOriginal) return;
 
             if (ModState.SuppressShowActorSequences)
             {
                 Mod.Log.Debug?.Write("Suppressing pathfinder updates and visiblity updates during damage call.");
-
-                ___markEffects.Remove(effect);
+                __instance.markEffects.Remove(effect);
 
                 if (effect.EffectData.targetingData.specialRules == AbilityDef.SpecialRules.Aura)
                 {
                     __instance.Combat.MessageCenter.PublishMessage(new AuraRemovedMessage(effect.creatorID, __instance.GUID, effect.id, effect.EffectData));
                 }
-                if (__instance.StealthPipsCurrent != ___StealthPipsPrevious)
+                if (__instance.StealthPipsCurrent != __instance.StealthPipsPrevious)
                 {
                     __instance.Combat.MessageCenter.PublishMessage(new StealthChangedMessage(__instance.GUID, __instance.StealthPipsCurrent));
                 }
--- FieldRepairs/FieldRepairs/Patches/UnitSpawnPointGameLogicPatches.cs
+++ FieldRepairs/FieldRepairs/Patches/UnitSpawnPointGameLogicPatches.cs
@@ -3,7 +3,7 @@
     [HarmonyPatch(typeof(UnitSpawnPointGameLogic), "Spawn")]
     public static class UnitSpawnPointGameLogic_Spawn
     {
-        public static void Prefix(ref bool __runOriginal, UnitSpawnPointGameLogic __instance, string ___teamDefinitionGuid)
+        public static void Prefix(ref bool __runOriginal, UnitSpawnPointGameLogic __instance)
         {
             if (!__runOriginal) return;
 
@@ -13,7 +13,7 @@
                 if (__instance.Combat.ActiveContract.ContractTypeValue.IsSkirmish)
                 {
                     Mod.Log.Debug?.Write($"Contract is skirmish. Existing tags are: {__instance.spawnEffectTags}");
-                    if (!__instance.Combat.HostilityMatrix.IsLocalPlayerFriendly(___teamDefinitionGuid))
+                    if (!__instance.Combat.HostilityMatrix.IsLocalPlayerFriendly(__instance.teamDefinitionGuid))
                     {
                         Mod.Log.Debug?.Write($"Unit belongs to enemy or neutral team, adding flag: {Mod.Config.Skirmish.Tag}");
                         __instance.spawnEffectTags.Add(Mod.Config.Skirmish.Tag);
